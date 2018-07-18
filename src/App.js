9d2b5ad28ca6d2dbd87085935c4cb47869b4ab1e
Ali Orlando <aorlando@onshift.com>
2018-07-18 16:34:11 -0400

    Create the redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './redux/reducers';
+import storeEnhancers from './redux/enhancers';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 
+const store = createStore(
+    rootReducer,
+    storeEnhancers
+);
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
-                <Route exact path="/" component={Home} />
-            </div>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                        </ul>
+                        <Route exact path="/" component={Home} />
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
